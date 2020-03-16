7e097bd2787c75f30bd23b1df85b3507b3f858b7
cuciocj <cuciocj@gmail.com>
2020-03-17 11:17:22 +1300

    added default login. Not much validation as of the moment.

--- src/main/java/a2i/sdi/chinchilla/rest/LoginController.java
+++ src/main/java/a2i/sdi/chinchilla/rest/LoginController.java
@@ -1,7 +1,12 @@
 package a2i.sdi.chinchilla.rest;
 
+import a2i.sdi.chinchilla.dao.UserDao;
+import a2i.sdi.chinchilla.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,19 +14,31 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class LoginController {
 
+    private UserDao userDao;
+
+    @Autowired
+    public LoginController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/login")
-    public String login(HttpServletRequest request) {
-        System.out.println("login()");
+    public String login(Model model, HttpServletRequest request) {
+        model.addAttribute("userLogin", new User());
         return "login";
     }
 
     @PostMapping("/login")
-    public String processLogin(HttpServletRequest request) {
-        if (request != null) {
-            System.out.println("request is not null " + request.getMethod());
-        } else {
-            System.out.println("request is null");
+    public String processLogin(@ModelAttribute User user, Model model) {
+        if(user.getUsername().length() > 0 && user.getPassword().length() > 0) {
+            user = userDao.find(user);
+            if(user != null) {
+                System.out.println(user.getId() + ":" + user.getUsername() + ":" + user.getPassword()
+                        + ":" + user.getEmail() + ":" + user.getFirstName() + ":" + user.getLastName());
+                return "feed";
+            }
         }
-        return "feed";
+
+        // todo: display if wrong credentials
+        return "login";
     }
 }
